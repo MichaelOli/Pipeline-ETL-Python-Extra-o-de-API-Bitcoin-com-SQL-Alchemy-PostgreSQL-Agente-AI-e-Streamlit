a06c000a937693c5e4d562233c2b311d65f5ea6b
Michael Oliveira Ribeiro <michael.ribeiro@bw.local>
2025-05-14 16:10:21 -0300

    Implementando o agente com Gemini

--- agents/agent_01.py
+++ agents/agent_01.py
@@ -1,16 +1,16 @@
 from agno.agent import Agent
-from agno.models.groq import Groq
+from agno.models.google import Gemini
 from agno.tools.duckduckgo import DuckDuckGoTools
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # Initialize the agent with an LLM via Groq and DuckDuckGoTools
 agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile"),
+    model=Gemini(id="gemini-1.5-flash"),
     description="Você é o melhor especialista de engenharia de dados do mundo!",
     tools=[DuckDuckGoTools()],      # Add DuckDuckGo tool to search the web
     show_tool_calls=True,           # Shows tool calls in the response, set to False to hide
@@ -18,4 +18,4 @@ agent = Agent(
 )
 
 # Prompt the agent to fetch a breaking news story from New York
-agent.print_response("Qual é o site da Jornada de dados?", stream=True)
+agent.tools.print_response("O que é o atacadão dia a dia?", stream=True)
