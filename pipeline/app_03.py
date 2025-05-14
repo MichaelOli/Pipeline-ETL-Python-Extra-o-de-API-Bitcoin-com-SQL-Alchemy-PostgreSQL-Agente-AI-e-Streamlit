9466a8542a520bd3fbb68a50e982395960bd44e1
Michael Oliveira Ribeiro <michael.ribeiro@bw.local>
2025-05-14 10:35:58 -0300

    Insert NOSQL - TinyDB

---
+++ pipeline/app_03.py
@@ -0,0 +1,33 @@
+import requests
+from tinydb import TinyDB
+from pprint import pprint
+
+
+
+def extrair():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return(response.json())
+
+def transformar(dados):
+    valor = dados['data']['amount'],
+    cripto = dados['data']['base'],
+    moeda = dados['data']['currency']
+    dados_tratados = {
+        "valor": valor,
+        "criptomoeda": cripto,
+        "moeda": moeda
+
+    }
+    return dados_tratados
+
+def load(dados_tratados):
+    db = TinyDB('db.json')
+    db.insert(dados_tratados)
+    print("Dados salvos com sucesso")
+
+if __name__ == "__main__":
+    dados = extrair()
+    dados_tratados = transformar(dados)
+    load(dados_tratados)
+    print(dados_tratados)
