0614df8636369ac646131af8703cca934cb2cc88
Michael Oliveira Ribeiro <michaeloliveira38@gmail.com>
2025-05-15 09:11:17 -0300

    Update agent_01.py

--- agents/agent_01.py
+++ agents/agent_01.py
@@ -6,15 +6,15 @@ import os
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
-# Initialize the agent with an LLM via Groq and DuckDuckGoTools
+
 agent = Agent(
     model=Gemini(id="gemini-1.5-flash"),
     description="Você é o melhor especialista de engenharia de dados do mundo!",
-    tools=[DuckDuckGoTools()],      # Add DuckDuckGo tool to search the web
-    show_tool_calls=True,           # Shows tool calls in the response, set to False to hide
-    markdown=True                   # Format responses in markdown
+    tools=[DuckDuckGoTools()],     
+    show_tool_calls=True,           
+    markdown=True                   
 )
 
 # Prompt the agent
